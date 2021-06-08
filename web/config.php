39137548861b594be63174b3442788cf16073f24
gtang8 <74369627+gtang8@users.noreply.github.com>
2021-06-08 14:31:28 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -5,5 +5,5 @@ define('GJ_CODE', getenv("GJ_CODE"));
 define('SITE_NAME',getenv("YouTibe"));
 define('TITLENAME', getenv("Youtube"));
 define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('EMAIL', getenv("gavingavintang@gmail.com"));
 ?>
